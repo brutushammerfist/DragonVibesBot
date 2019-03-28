0a06b9c9f29c52a4ac02df0902aada476fc51ff5
EC2 Default User <ec2-user@ip-172-31-39-65.us-east-2.compute.internal>
2019-03-28 01:33:29 +0000

    i may or may not have installed php on apache server...

--- webhookHandler.php
+++ webhookHandler.php
@@ -7,11 +7,8 @@
         $res = $_GET['hub_challenge'];
         echo $res;
     } else {
-        echo $_GET;
         http_response_code(200);
     }
-    
-    echo "bruh";
     
     $webhookResponse = json_decode(file_get_contents('php://input'), true);
     

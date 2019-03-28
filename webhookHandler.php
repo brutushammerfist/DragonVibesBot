cb3451ac487d6c4c54bd0f3a6d93627c9a60047d
EC2 Default User <ec2-user@ip-172-31-39-65.us-east-2.compute.internal>
2019-03-28 22:27:40 +0000

    still testing...

--- webhookHandler.php
+++ webhookHandler.php
@@ -5,6 +5,7 @@
 <?php
     if(isset($_GET['hub_challenge']) && !empty($_GET['hub_challenge'])){
         $res = $_GET['hub_challenge'];
+        http_response_code(200);
         echo $res;
     } else {
         http_response_code(200);
@@ -17,7 +18,7 @@
         fwrite($webRes, json_encode($webhookResponse));
         fclose($webRes);
         
-        $discordNotif = escapeshellcmd('python3 ./discordNotif.py');
+        $discordNotif = escapeshellcmd('python3 ./discordNotif.py > /tmp/dumb.log 2> /tmp/dumb.log');
         exec($discordNotif);
     }
 ?>

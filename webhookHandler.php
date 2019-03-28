d41f56ec068cd20ea211648ee4dabd62356baaef
EC2 Default User <ec2-user@ip-172-31-39-65.us-east-2.compute.internal>
2019-03-28 00:43:37 +0000

    removing form from html

--- webhookHandler.php
+++ webhookHandler.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
 <html>
 <body>
-    
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    Name: <input type="text" name="fname">
-    <input type="submit">
-</form>
 
 <?php
     if(isset($_GET['hub_challenge']) && !empty($_GET['hub_challenge'])){

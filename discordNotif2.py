c54713c187d893039d0b901acf0c1a03d11294d2
EC2 Default User <ec2-user@ip-172-31-39-65.us-east-2.compute.internal>
2019-03-29 00:19:22 +0000

    student indents

--- discordNotif2.py
+++ discordNotif2.py
@@ -1,19 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from urlparse import urlparse
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):"""
-        query = urlparse(self.path).query
-        query_components = dict(qc.split("=") for qc in query.split("&"))
-        
-        self.end_headers()
-        
-        if (query_components["hub.challenge"]) != None:
-            self.send_response(200)
-            self.wfile.write(query_components["hub.challenge"])
-        else:
-            self.send_response(200)
-            self.wfile.write(OK)"""
+    def do_GET(self):
         content_length = int(self.headers['Content-Length'])
         get_data = self.rfile.read(content_length)
         
@@ -26,7 +14,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.wfile.write("OK")
         
-    def do_POST(self):
+    #def do_POST(self):
         
         
 httpd = HTTPServer(('localhost', 8080), SimpleHTTPRequestHandler)

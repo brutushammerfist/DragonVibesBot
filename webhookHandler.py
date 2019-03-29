0e14f7f3d435cda6f6259061a139ed78238c78a7
EC2 Default User <ec2-user@ip-172-31-39-65.us-east-2.compute.internal>
2019-03-29 01:33:59 +0000

    renaming variables and filenames to be more meaningful

--- webhookHandler.py
+++ webhookHandler.py
@@ -5,14 +5,14 @@ import json
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         query = urlparse(self.path).query
-        query_components = dict(qc.split("=") for qc in query.split("&"))
+        query_parameters = dict(qc.split("=") for qc in query.split("&") if "=" in qc)
         
         print(self.path)
         
-        if (query_components["hub.challenge"]) != None:
+        if (query_parameters["hub.challenge"]) != None:
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(query_components["hub.challenge"].encode("UTF-8"))
+            self.wfile.write(query_parameters["hub.challenge"].encode("UTF-8"))
             #self.wfile.close()
         else:
             self.send_response(200)
@@ -25,6 +25,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         body = self.rfile.read(content_length).decode("UTF-8")
         body = json.loads(body)
         
-httpd = HTTPServer(('0.0.0.0', 8080), SimpleHTTPRequestHandler)
+#httpd = HTTPServer(('0.0.0.0', 8080), SimpleHTTPRequestHandler)
 
-httpd.serve_forever()
+#httpd.serve_forever()

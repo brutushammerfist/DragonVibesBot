2776e54a2cd2407510805afd31615f6be43749ef
EC2 Default User <ec2-user@ip-172-31-39-65.us-east-2.compute.internal>
2019-03-29 00:17:03 +0000

    screw php, back to python!

---
+++ discordNotif2.py
@@ -0,0 +1,34 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urlparse import urlparse
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):"""
+        query = urlparse(self.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
+        
+        self.end_headers()
+        
+        if (query_components["hub.challenge"]) != None:
+            self.send_response(200)
+            self.wfile.write(query_components["hub.challenge"])
+        else:
+            self.send_response(200)
+            self.wfile.write(OK)"""
+        content_length = int(self.headers['Content-Length'])
+        get_data = self.rfile.read(content_length)
+        
+        print(get_data)
+        
+        if(get_data["hub.challenge"]) != None:
+            self.send_response(200)
+            self.wfile.write(get_data["hub.challenge"])
+        else:
+            self.send_response(200)
+            self.wfile.write("OK")
+        
+    def do_POST(self):
+        
+        
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+
+httpd.serve_forever()

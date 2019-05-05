77b21c7b906b51b3b911864cf8b98d58c4098971
BrutusHammerfist <mperr006@odu.edu>
2019-05-05 22:30:46 +0000

    missing paranthesis

--- DragonVibesBot.py
+++ DragonVibesBot.py
@@ -71,7 +71,7 @@ class Bot(commands.Bot):
         subscribe = requests.post('https://api.twitch.tv/helix/webhooks/hub', headers=headers, data=json.dumps(payload))
         print(subscribe.content)
         def httpMain():
-            httpd = HTTPServer(('0.0.0.0', 8004, SimpleHTTPRequestHandler)#int(Port)), SimpleHTTPRequestHandler)
+            httpd = HTTPServer(('0.0.0.0', 8004), SimpleHTTPRequestHandler)#int(Port)), SimpleHTTPRequestHandler)
             httpd.serve_forever()
         
         webhookThread = threading.Thread(target=httpMain)

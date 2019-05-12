b0f9ef5082cfd1916484f1019c5693fe70adb414
BrutusHammerfist <mperr006@odu.edu>
2019-05-12 20:02:22 +0000

    stupid version differences

--- webserver/webpageForm.py
+++ webserver/webpageForm.py
@@ -149,7 +149,7 @@ class CustomServerHandler(http.server.BaseHTTPRequestHandler):
         if ctype == 'multipart/form-data':
             postvars = cgi.parse_multipart(self.rfile, pdict)
         elif ctype == 'application/x-www-form-urlencoded':
-            length = int(self.headers.getheader('content-length'))
+            length = int(self.headers.get('content-length'))
             postvars = cgi.parse_qs(
                 self.rfile.read(length), keep_blank_values=1)
         else:

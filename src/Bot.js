8f86a3f0f60ae0325bc67cddbaaeab4f3cf850f9
brutushammerfist <mperr006@odu.edu>
2021-10-18 10:42:59 -0400

    fix circular dependency

--- src/Bot.js
+++ src/Bot.js
@@ -3,13 +3,7 @@ const TwitchAPI = require('./TwitchAPI.js');
 const WS = require('./WS.js');
 
 class Bot {
-    constructor() {
-        this.commands = new Map();
-        this.giveawayActive = false;
-        this.poolActive = false;
-        this.giveawayPool = [];
-        this.poolPool = [];
-    }
+    constructor() { }
 
     handleCommand(commandName, username) {
         switch (commandName) {
@@ -20,102 +14,26 @@ class Bot {
             case "coins":
                 return username + " has accumulated " + Database.getCoins(username) + "coins in their hoard!";
             case "openga":
-                return this.openGiveaway();
+                return Giveaway.openGiveaway();
             case "openpool":
-                return this.openPool();
+                return Giveaway.openPool();
             case "enter":
-                return this.enterGiveaway(username);
+                //return Giveaway.enterGiveaway(username);
+                var response = Giveaway.enterGiveaway(username);
+                if (response.includes("successfully")) {
+                    WS.broadcastMessage(JSON.stringify({ giveawayEntries: Giveaway.giveawayPool }));
+                }
+                return response;
             case "pool":
-                return this.enterPool(username);
+                //return Giveaway.enterPool(username);
+                var response = Giveaway.enterPool(username);
+                if (response.includes("successfully")) {
+                    WS.broadcastMessage(JSON.stringify({ poolEntries: Giveaway.poolPool }));
+                }
+                return response;
             default:
                 return "Invalid Command!";
         }
-    }
-
-    /****
-     * Giveaway Functions
-     */
-
-    openGiveaway() {
-        if (this.giveawayActive) {
-            return "Giveaway is already active!";
-        }
-
-        this.giveawayActive = true;
-        return "Giveaway started!";
-    }
-
-    openPool() {
-        if (this.poolActive) {
-            return "Pool is already open!";
-        }
-
-        this.poolActive = true;
-        return "Pool opened!";
-    }
-
-    enterGiveaway(username) {
-        if (this.giveawayActive) {
-            if (!this.giveawayPool.includes(username)) {
-                this.giveawayPool.push(username);
-
-                WS.broadcastMessage(JSON.stringify({ giveawayEntries: this.giveawayPool }));
-
-                return `@${username}, you've successfully been entered into the giveaway.`;
-            }
-
-            return `@${username}, you've already entered the giveaway.`;
-        }
-
-        return "There is not an active giveaway.";
-    }
-
-    enterPool(username) {
-        if (this.poolActive) {
-            if (!this.poolPool.includes(username)) {
-                this.poolPool.push(username);
-
-                WS.broadcastMessage(JSON.stringify({ poolEntries: this.poolPool }));
-
-                return `@${username}, you've successfully been entered into the pool.`;
-            }
-
-            return `@${username}, you've already entered the pool.`;
-        }
-
-        return "There is not an active pool.";
-    }
-
-    pullGiveaway() {
-        var winner = this.giveawayPool[Math.floor(Math.random() * this.giveawayPool.length)];
-        this.removeGiveawayEntry(winner);
-        return winner;
-    }
-
-    clearGiveaway() {
-        this.giveawayPool = [];
-        WS.broadcastMessage("clear-giveaway");
-    }
-
-    removeGiveawayEntry(username) {
-        this.giveawayPool.splice(this.giveawayPool.indexOf(username), 1);
-        WS.broadcastMessage(JSON.stringify({ giveawayEntries: this.giveawayPool }));
-    }
-
-    pullPool() {
-        var winner = this.poolPool[Math.floor(Math.random() * this.poolPool.length)];
-        this.removePoolEntry(winner);
-        return winner;
-    }
-
-    clearPool() {
-        this.poolPool = [];
-        WS.broadcastMessage("clear-pool");
-    }
-
-    removePoolEntry() {
-        this.poolPool.splice(this.poolPool.indexOf(username), 1);
-        WS.broadcastMessage(JSON.stringify({ poolEntries: this.poolPool }));
     }
 }
 
--- src/WS.js
+++ src/WS.js
@@ -2,7 +2,6 @@ const { readFileSync } = require('fs');
 const { createServer } = require('https');
 const { WebSocketServer } = require('ws');
 const secrets = require('../secrets.json');
-const WSMsgHandler = require('./WSMsgHandler');
 
 class WS {
     constructor() {
@@ -17,7 +16,7 @@ class WS {
             this.ws = ws;
 
             this.ws.on('message', function incoming(message) {
-                WSMsgHandler.handleMessage(message);
+                this.handleMessage(message);
             });
 
             this.ws.send('something');
@@ -36,6 +35,41 @@ class WS {
                 client.send(message);
             }
         });
+    }
+
+    handleMessage(message) {
+        console.log('received: %s', message);
+
+        switch (message) {
+            case "clear-giveaway":
+                Giveaway.clearGiveaway();
+                this.broadcastMessage("clear-giveaway");
+                break;
+            case "clear-pool":
+                Giveaway.clearPool();
+                this.broadcastMessage("clear-pool");
+                break;
+            case "pull-giveaway":
+                Giveaway.pullGiveaway();
+                break;
+            case "pull-pool":
+                Giveaway.pullPool();
+                break;
+            default:
+                var data = JSON.parse(message);
+
+                console.log(data);
+
+                if (data.removeGiveaway) {
+                    Giveaway.removeGiveawayEntry(data.removeGiveaway);
+                    this.broadcastMessage(JSON.stringify({ giveawayEntries: Giveaway.giveawayPool }));
+                }
+
+                if (data.removePool) {
+                    Giveaway.removePoolEntry(data.removePool);
+                    this.broadcastMessage(JSON.stringify({ giveawayEntries: Giveaway.poolPool }));
+                }
+        };
     }
 }
 

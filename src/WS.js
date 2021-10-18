ab9a19cfb80410651d5825862c82b0edc83eb163
brutushammerfist <mperr006@odu.edu>
2021-10-17 21:31:41 -0400

    update websocket to use https

--- src/WS.js
+++ src/WS.js
@@ -1,8 +1,10 @@
+const { readFileSync } = require('fs');
+const { createServer } = require('https');
 const { WebSocketServer } = require('ws');
 
 class WS {
     constructor() {
-        this.wss = new WebSocketServer({ port: 8080, host: "brutus.dev", path: "/socket" });
+        /*this.wss = new WebSocketServer({ port: 8080, host: "brutus.dev", path: "/socket" });
 
         this.wss.on('connection', function connection(ws) {
             ws.on('message', function incoming(message) {
@@ -14,7 +16,27 @@ class WS {
 
         this.wss.on('listening', function () {
             console.log("Websocket Server Started");
+        });*/
+
+        this.server = createServer({
+            cert: readFileSync("/etc/letsencrypt/live/brutus.dev/fullchain.pem"),
+            key: readFileSync("/etc/letsencrypt/live/brutus.dev/privkey.pem")
         });
+        this.wss = new WebSocketServer({ server: this.server });
+
+        this.wss.on('connection', function connection(ws) {
+            ws.on('message', function incoming(message) {
+                console.log('received: %s', message);
+            });
+
+            ws.send('something');
+        });
+
+        this.wss.on('listening', function () {
+            console.log("Websocket Server Started");
+        });
+
+        this.server.listen(8080);
     }
 }
 
